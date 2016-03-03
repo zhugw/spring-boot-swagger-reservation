c3e3062276ad4a52fe98d3875f0e32fabe310b81
zhugw <13656635451@qq.com>
2016-03-03 15:29:58 +0800

    Update test-user-data.sql
    屏蔽敏感信息

--- src/test/resources/test-user-data.sql
+++ src/test/resources/test-user-data.sql
@@ -1,8 +1,8 @@
-insert into wiki_user(name, email) values('foo','foo@helijia.com');
-insert into wiki_user(name, email) values('bar','bar@helijia.com');
-insert into wiki_user(name, email) values('test_add_reservation','test_add_reservation@helijia.com');
-insert into wiki_user(name, email) values('test_repeat_add_reservation','test_repeat_add_reservation@helijia.com');
-insert into wiki_user(name, email) values('test_updateReservation','test_updateReservation@helijia.com');
-insert into wiki_user(name, email) values('test_deleteReservation','test_deleteReservation@helijia.com');
-insert into wiki_user(name, email) values('test_plus1','test_plus1@helijia.com');
-insert into wiki_user(name, email) values('test_deleteReservation_by_name','test_deleteReservation_by_name@helijia.com');
+insert into wiki_user(name, email) values('foo','foo@foobar.com');
+insert into wiki_user(name, email) values('bar','bar@foobar.com');
+insert into wiki_user(name, email) values('test_add_reservation','test_add_reservation@foobar.com');
+insert into wiki_user(name, email) values('test_repeat_add_reservation','test_repeat_add_reservation@foobar.com');
+insert into wiki_user(name, email) values('test_updateReservation','test_updateReservation@foobar.com');
+insert into wiki_user(name, email) values('test_deleteReservation','test_deleteReservation@foobar.com');
+insert into wiki_user(name, email) values('test_plus1','test_plus1@foobar.com');
+insert into wiki_user(name, email) values('test_deleteReservation_by_name','test_deleteReservation_by_name@foobar.com');
